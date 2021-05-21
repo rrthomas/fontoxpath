2f14571b246589db9cf21d770e0eb372cc2348b5
devatwork <b.willems@premotion.nl>
2021-05-21 09:57:00 +0200

    Improve the crash report formatting
    The new format makes it easier to copy & paste the crash report into
    a GitHub issue because it includes Markdown formatting.

--- fuzzers/engine.ts
+++ fuzzers/engine.ts
@@ -67,7 +67,7 @@ export default class Engine<TFuzzer extends IFuzzer> {
 
 						// Print the error
 						process.stdout.write(
-							`\n\n!!! \x1b[31mCrash found\x1b[0m !!!\nSelector: ${msg.selector}\nLanguage: ${msg.language}\n${msg.stack}\n\n`
+							`\n\n!!! \x1b[31mCrash found\x1b[0m !!!\nSelector: \`${msg.selector}\`\nLanguage: \`${msg.language}\`\n\`\`\`\n${msg.stack}\n\`\`\`\n\n`
 						);
 						break;
 					}
