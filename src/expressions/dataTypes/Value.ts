4aeffb4bf9d37884c815b315087fb7dd11ef00dc
Reuben Thomas <rrt@sc3d.org>
2021-09-06 21:14:47 +0100

    Implement fn:error() in one- and two-argument forms

--- src/expressions/dataTypes/Value.ts
+++ src/expressions/dataTypes/Value.ts
@@ -244,6 +244,7 @@ const stringToValueTypeMap: { [key: string]: ValueType } = {
 	'function(*)': ValueType.FUNCTION,
 	'map(*)': ValueType.MAP,
 	'array(*)': ValueType.ARRAY,
+	none: ValueType.NONE,
 };
 
 /**
